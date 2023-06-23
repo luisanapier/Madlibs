e632fcb36a51fd060bbb4d1edf70376e76913ae8
Luisa Napier <luisanapier@Luisas-MacBook-Pro.local>
2023-06-22 22:17:40 -0700

    Game completed

--- madlibs.py
+++ madlibs.py
@@ -50,6 +50,29 @@ def greet_person():
 
     return render_template("compliment.html", person=player, compliment=compliment)
 
+@app.route("/game")
+def show_madlib_form():
+
+    answer = request.args.get("answer")
+
+    if answer == "yes":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+    
+@app.route("/madlib")
+def show_madlib():
+
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+    
+    return render_template("madlib.html", 
+                           person=person,
+                           color=color,
+                           noun=noun,
+                           adjective=adjective)
 
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
